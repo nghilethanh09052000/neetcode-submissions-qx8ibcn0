4179f7cbddea6886324d72b8cc2f5a8e831ca1f7
Nghi Le Thanh <nghilt19411@gmail.com>
2026-06-13 14:24:00 +0700

    Add: sql-game-play-analysis-iii - submission-1

---
+++ SQL/sql-game-play-analysis-iii/submission-1.sql
@@ -0,0 +1,7 @@
+-- Write your query below
+select
+    player_id,
+    event_date,
+    sum(games_played) over (partition by player_id order by
+    event_date asc) as games_played_so_far
+from activity
